c2e2fd6ce718ec6a8ed1435c9bf36fec88159b56
Sean <sturner@nmt.edu>
2015-06-13 14:26:00 -0600

    ugh still not linking

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,9 @@
 -- from ch05 of Real World Haskell
 -- Main.hs
 
-module Main () where
+module Main where
 
 import SimpleJSON
 
+main :: IO ()
 main = print (JObject [("foo", JNumber 1), ("bar", JBool False)])
