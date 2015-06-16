649ade17a7e8b3c2911ca376afa9925f1e74edea
Sean Turner <sturner@nmt.edu>
2015-06-16 15:36:44 -0600

    add helpers for pretty printing strings

--- src/PrettyJSON.hs
+++ src/PrettyJSON.hs
@@ -4,6 +4,7 @@
 module PrettyJSON where
 
 import           PrettyStub
+import           SimpleJSON
 
 renderJSONValue :: JSONValue -> Doc
 renderJSONValue (JSONBool True)     = text "true"
@@ -11,3 +12,10 @@ renderJSONValue (JSONBool False)    = text "false"
 renderJSONValue JSONNull            = text "null"
 renderJSONValue (JSONNumber num)    = double num
 renderJSONValue (JSONString str)    = string str
+
+prettyPrintString :: String -> Doc
+prettyPrintString = enclose '"' '"' . hcat . map oneChar
+
+-- helpers for pretty printing a string
+enclose :: Char -> Char -> Doc -> Doc
+enclose left right x = char left <> x <> char right
